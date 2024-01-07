a33f852c63049e776f035dd17aaa0d66c1b781f1
Björn Büttner <Idrinth@users.noreply.github.com>
2024-01-07 09:16:09 +0100

    fix return code

--- bin/lint-css-duplicates.js
+++ bin/lint-css-duplicates.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import lint from '../src/index.js';
 
-process.exit(lint(process.argv[2] || process.cwd()) ? 1 : 0);
+process.exit(lint(process.argv[2] || process.cwd()) ? 0 : 1);
